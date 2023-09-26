9b871dceb8c19714a946a11a92cb59274825b5a0
Ganesh kumar <137510370+Ganeshkumar-06@users.noreply.github.com>
2023-09-26 22:19:32 +0530

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -7,6 +7,7 @@ void swap(&a,&b){
 }
 void main(){
   int num1,num2;
+  scanf("%d %d",&num1,&num2);
   printf("Numbers Before Swapping\vNum1 : %d\tNum2 : %d",num1,num2);
   swap(num1,num2);
   printf("Swapped Numbers\vNum1 : %d\tNum2 : %d",num1,num2);
